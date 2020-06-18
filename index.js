b6dc762f213bf3f4d9c4dea24affe3e149ce9d25
Tristan Guest <tristanguest@gmail.com>
2020-06-18 19:43:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 
+function getFirstSelector(selector) {
+  var sel = document.querySelectorAll(selector)
+  return sel[0]
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested').querySelector('.target')
+}
+
+function increaseRankBy(n) {
+  let rlist = document.querySelectorAll('.ranked-list li')
+  for (let i = 0; i < rlist.length; i++) {
+    rlist[i].innerHTML = parseInt(rlist[i].innerHTML) + n
+  }
+  return rlist
+}
+
+function deepestChild() {
+  let gNode = document.querySelector('#grand-node')
+  
+  let current = gNode
+  let last
+  
+  while (current) {
+    last = current
+    current = current.querySelector('div')
+  }
+  
+  return last
+}
